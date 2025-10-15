5c529991c9a80238ef44200f0b45b7fc3027f450
ongoing-z <code@ongoingblog.org>
2025-10-15 10:37:08 +0800

    优化测试程序：并发消费、顺序消费。

--- example/src/main/java/org/apache/rocketmq/example/simple/PushConsumerOrderly.java
+++ example/src/main/java/org/apache/rocketmq/example/simple/PushConsumerOrderly.java
@@ -16,32 +16,37 @@
  */
 package org.apache.rocketmq.example.simple;
 
-import java.util.List;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
-import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.consumer.listener.*;
 import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
 
-public class PushConsumer {
+import java.util.List;
+
+/**
+ * 顺序消费测试。
+ */
+public class PushConsumerOrderly {
 
     public static void main(String[] args) throws InterruptedException, MQClientException {
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("CID_JODIE_1");
-        consumer.subscribe("TopicTest", "*");
-        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-        //wrong time format 2017_0422_221800
-        consumer.setConsumeTimestamp("20181109221800");
-        consumer.registerMessageListener(new MessageListenerConcurrently() {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(
+                "PushConsumer001");
+        consumer.setNamesrvAddr("localhost:9876");
+        // 订阅 topic
+        consumer.subscribe("MyProducerBatchGroup", "*"); // * 代表接收所有。
+
+        consumer.registerMessageListener(new MessageListenerOrderly() {
 
             @Override
-            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
                 System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
-                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                return ConsumeOrderlyStatus.SUCCESS;
             }
         });
+
         consumer.start();
         System.out.printf("Consumer Started.%n");
+
+        consumer.shutdown();
     }
 }
